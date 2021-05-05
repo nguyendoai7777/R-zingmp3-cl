463f04b06c64b2db242f99284888ca27a0f2c22e
Doai Nguyen <doainguyen@tdt.asia>
2021-05-05 17:37:26 +0700

    doing slider prev

--- apps/ui/src/app/components/slide-gallery/slide-gallery.tsx
+++ apps/ui/src/app/components/slide-gallery/slide-gallery.tsx
@@ -5,18 +5,39 @@ import { GALLERY_DATA } from './data';
 
 const SlideGallery = () => {
   const galleryParent = React.useRef(null);
+
   useEffect(() => {
+    let index = 1;
     const gallery = document.querySelectorAll('.gallery-data');
-    gallery[1].classList.add('item-prev');
-    gallery[3].classList.add('item-next');
-    gallery[2].classList.add('item-selected');
-    gallery[4].classList.add('item-hidden');
-    gallery[0].classList.add('item-hidden');
-    setInterval(() => {
+    const resetClass = () => {
       gallery.forEach(e => {
+        e.classList.remove('item-prev');
         e.classList.remove('item-selected');
+        e.classList.remove('item-next');
+        e.classList.remove('item-hidden');
       });
-    }, 1000);
+    };
+    const nextImage = () => {
+      index++;
+      resetClass();
+      gallery[(index - 1) % 5].classList.add('item-prev');
+      gallery[(index) % 5].classList.add('item-selected');
+      gallery[(index + 1) % 5].classList.add('item-next');
+      gallery[(index + 2) % 5].classList.add('item-hidden');
+      gallery[(index + 3) % 5].classList.add('item-hidden');
+    };
+    const prevImage = () => {
+      index--;
+      resetClass();
+      gallery[Math.abs((index - 1)) % 5].classList.add('item-prev');
+      gallery[Math.abs(index) % 5].classList.add('item-selected');
+      gallery[Math.abs((index + 1) % 5)].classList.add('item-next');
+      gallery[Math.abs((index + 2) % 5)].classList.add('item-hidden');
+      gallery[Math.abs((index + 3) % 5)].classList.add('item-hidden');
+    };
+    setInterval(() => {
+      prevImage();
+    }, 2500);
     console.log(gallery);
   }, []);
 
@@ -24,19 +45,19 @@ const SlideGallery = () => {
     <div className='gallery-wrapper'>
       <div className='gallery-images' ref={galleryParent}>
 
-        <div className='gallery-data '>
+        <div className='gallery-data item-prev'>
           <img src={GALLERY_DATA[1].imgUrl} alt='' />
         </div>
-        <div className='gallery-data '>
+        <div className='gallery-data item-selected'>
           <img src={GALLERY_DATA[4].imgUrl} alt='' />
         </div>
-        <div className='gallery-data '>
+        <div className='gallery-data item-next'>
           <img src={GALLERY_DATA[2].imgUrl} alt='' />
         </div>
-        <div className='gallery-data'>
+        <div className='gallery-data item-hidden'>
           <img src={GALLERY_DATA[3].imgUrl} alt='' />
         </div>
-        <div className='gallery-data'>
+        <div className='gallery-data item-hidden'>
           <img src={GALLERY_DATA[0].imgUrl} alt='' />
         </div>
         {/*<div className='gallery-controller controller-btn npd-icon-btn btn-prev'>
